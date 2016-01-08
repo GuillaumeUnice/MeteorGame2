9212116d52e2416003a12e5c193fed0b96c6e3ec
Guillaume BORG <guillaume.unice@gmail.com>
2016-01-08 13:40:25 +0100

    integration leap

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -323,6 +323,7 @@ function keyInput(event) {
 
 // Function called when a key is pressed, will change direction if arrow key.
 function directionDown(event) {
+    console.log(event);
 	var key = event.which || event.keyCode;
 
 	if (directional(key)) {
@@ -953,3 +954,98 @@ function resize() {
     player.screenHeight = c.height = screenHeight = playerType == 'player' ? window.innerHeight : gameHeight;
     socket.emit('windowResized', { screenWidth: screenWidth, screenHeight: screenHeight });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var cats = {};
+
+Leap.loop(function(frame) {
+
+  frame.hands.forEach(function(hand, index) {
+    console.log(hand.screenPosition());/*
+    var cat = ( cats[index] || (cats[index] = new Cat()) );    
+    cat.setTransform(hand.screenPosition(), hand.roll());
+*/
+      /*target.x = 3;//mouse.clientX - screenWidth / 2;
+      target.y = 3;    
+      socket.emit('0', target); */
+      //directionDown("keydown charCode=0, keyCode=40");
+      //console.log(event);
+      var key = 0;
+      if(hand.screenPosition()[0] >= 700) {
+        key = 40;//event.which || event.keyCode;    
+      } else {
+        key = 38;
+      }
+    
+    console.log('ooooooooooooooooooooooooook : ' + key);
+    if (key) {
+        console.log('ooooooooooooooooooooooooook');
+        directionLock = true;
+        if (newDirection(key,directions, true)) {
+
+            updateTarget(directions);
+            socket.emit('0', target);
+        }
+    }
+  });
+  
+}).use('screenPosition', {scale: 0.25});
+
+
+var Cat = function() {
+  var cat = this;
+  var img = document.createElement('img');
+  img.src = 'vaisseaux.png';
+  img.style.position = 'absolute';
+  img.onload = function () {
+    cat.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
+    document.body.appendChild(img);
+  };
+  
+  cat.setTransform = function(position, rotation) {
+
+    img.style.left = position[0] - img.width  / 2 + 'px';
+    img.style.top  = position[1] - img.height / 2 + 'px';
+
+    img.style.transform = 'rotate(' + -rotation + 'rad)';
+    
+    img.style.webkitTransform = img.style.MozTransform = img.style.msTransform =
+    img.style.OTransform = img.style.transform;
+
+  };
+
+};
+
+cats[0] = new Cat();
+
+// This allows us to move the cat even whilst in an iFrame.
+Leap.loopController.setBackground(true);
