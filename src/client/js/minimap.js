a9a4d1da03d828402c01d24c4e5e3c28675346ff
Guillaume BORG <guillaume.unice@gmail.com>
2016-01-15 12:32:08 +0100

    Add minimap

---
+++ src/client/js/minimap.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('mini_cvs');
+canvas.width = screenWidth / 4; canvas.height = screenHeight / 4;
+
+var destCtx = canvas.getContext('2d');
+
+destCtx.drawImage(c, 0, 0);
+
