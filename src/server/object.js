d689b4c5e041fb27788bce51eae37dd2a820f516
Falou SECK <mfalouseck@gmail.com>
2016-01-31 23:10:18 +0100

    Code cleaning

--- src/server/object.js
+++ src/server/object.js
@@ -3,40 +3,40 @@ var util = require('./lib/util');
 
 
 /**
-    Fonction permettant d'ajouter un objet : position, taille et couleur
-**/
-exports.addObject = function (toAdd, c, object) {
-    var radius = c.object.objectRadius;
+ Fonction permettant d'ajouter un objet : position, taille et couleur
+ **/
+exports.addObject = function (toAdd, configuration, object) {
+    var radius = configuration.object.objectRadius;
     while (toAdd--) {
 
-        var position = c.foodUniformDisposition ? util.uniformPosition(object, radius) : util.randomPosition(radius);
+        var position = configuration.foodUniformDisposition ? util.uniformPosition(object, radius) : util.randomPosition(radius);
         var random = Math.floor(Math.random() * 3) + 1;
-        if(random === 1) {
+        if (random === 1) {
             object.push({
                 // Make IDs unique.
                 id: ((new Date()).getTime() + '' + object.length) >>> 0,
                 x: position.x,
                 y: position.y,
-                radius: c.object.objectRadius,
-                hue: c.object.mineType.fill,
-                fill: c.object.mineType.fill,
-                stroke: c.object.mineType.stroke,
-                strokeWidth: c.object.strokeWidth,
-                type: c.object.mineType.name,
+                radius: configuration.object.objectRadius,
+                hue: configuration.object.mineType.fill,
+                fill: configuration.object.mineType.fill,
+                stroke: configuration.object.mineType.stroke,
+                strokeWidth: configuration.object.strokeWidth,
+                type: configuration.object.mineType.name,
                 imageUrl: 'img/bomb.png'
             });
-        } else if(random === 2) {
+        } else if (random === 2) {
             object.push({
                 // Make IDs unique.
                 id: ((new Date()).getTime() + '' + object.length) >>> 0,
                 x: position.x,
                 y: position.y,
-                radius: c.object.objectRadius,
-                hue: c.object.bulletType.fill,
-                fill: c.object.bulletType.fill,
-                stroke: c.object.bulletType.stroke,
-                strokeWidth: c.object.strokeWidth,
-                type: c.object.bulletType.name,
+                radius: configuration.object.objectRadius,
+                hue: configuration.object.bulletType.fill,
+                fill: configuration.object.bulletType.fill,
+                stroke: configuration.object.bulletType.stroke,
+                strokeWidth: configuration.object.strokeWidth,
+                type: configuration.object.bulletType.name,
                 imageUrl: 'img/treature.png'
             });
         } else {
@@ -45,38 +45,38 @@ exports.addObject = function (toAdd, c, object) {
                 id: ((new Date()).getTime() + '' + object.length) >>> 0,
                 x: position.x,
                 y: position.y,
-                radius: c.object.objectRadius,
-                hue: c.object.lifeType.fill,
-                fill: c.object.lifeType.fill,
-                stroke: c.object.lifeType.stroke,
-                strokeWidth: c.object.strokeWidth,
-                type: c.object.lifeType.name,
+                radius: configuration.object.objectRadius,
+                hue: configuration.object.lifeType.fill,
+                fill: configuration.object.lifeType.fill,
+                stroke: configuration.object.lifeType.stroke,
+                strokeWidth: configuration.object.strokeWidth,
+                type: configuration.object.lifeType.name,
                 imageUrl: 'img/life.png'
             });
         }
-        
+
     }
 
 };
 
 
 /**
-    Fonction s'occupant de la generation d'objet en fonction des objets restant
-    fait un ratio entre objet max et objet restant.
-**/
+ Fonction s'occupant de la generation d'objet en fonction des objets restant
+ fait un ratio entre objet max et objet restant.
+ **/
 
 /** Simple suppression d'objet **/
-exports.removeObject = function(toRem) {
-    while (toRem--) {
+exports.removeObject = function (toRemove) {
+    while (toRemove--) {
         Object.pop();
     }
 };
 /** Test de collision **/
-exports.funcObject = function (f) {
-    return SAT.pointInCircle(new SATVector(f.x, f.y), playerCircle);
+exports.funcObject = function (object) {
+    return SAT.pointInCircle(new SATVector(object.x, object.y), playerCircle);
 };
 /** Suppression d'objet **/
-exports.deleteObject = function (f) {
-    object[f] = {};
-    object.splice(f, 1);
+exports.deleteObject = function (object) {
+    object[object] = {};
+    object.splice(object, 1);
 };
